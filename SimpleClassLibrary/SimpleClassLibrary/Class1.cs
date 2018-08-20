12051c10bfd090a6e5b9e44aadf8207b8cb699a8
Dushyant Priyadarshee <dushyant.priyadarshee@sharp-ls.eu>
2018-08-20 11:31:27 +0100

    Add SimpleClassLibrary.

---
+++ SimpleClassLibrary/SimpleClassLibrary/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SimpleClassLibrary
+{
+    public class Class1
+    {
+        public void DisplayMessage()
+        {
+            int i = 0;
+            int j = 1;
+            int result = i + j;
+
+            Console.WriteLine("In SimpleClassLibrary.");
+            Console.WriteLine("Result = " + result);
+        }
+    }
+}
